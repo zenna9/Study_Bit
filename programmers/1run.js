a08118dedb5581a99168a17df303f3355650765b
zenna <97446606+zenna9@users.noreply.github.com>
2023-05-29 23:53:55 +0900

    힘들다진짜..

--- programmers/1run.js
+++ programmers/1run.js
@@ -18,32 +18,74 @@ function solution(players, callings) {
     //     }
     // }
 
+    ////3번째 시도 실패================
+    // let counters = {};
+    // callings.forEach(i => {
+    //     if(counters[i] == undefined){
+    //         counters[i] = 1;
+    //     }else{
+    //         counters[i] += 1;
+    //     }
+    // });
+
+    // Object.keys(counters).forEach((ite)=>{
+    //     let frontplayers = [];  
+    //     const indexq = players.indexOf(ite);
+    //     const indexx = indexq - counters[ite];
+    //     players.map((item, index)=>{
+    //         if(index < indexx){
+    //             frontplayers.push(item);
+    //         }else if(index == indexx){
+    //             frontplayers.push(ite);
+    //             frontplayers.push(item);
+    //         }else if(index != indexq){
+    //             frontplayers.push(item);
+    //         }
+    //     })
+    //     players = frontplayers;
+    // })
+    // return players;
+
+    ////4번째 시도================================================================속상쓰..
+    // callings.forEach(cai => {
+    //     let farr = [];
+    //     for (let i = 1; i < players.length; i++) {
+    //         if( players[i] == cai){
+    //             farr.push(cai, players[i-1]);
+    //             i++;
+    //         }else{
+    //             farr.push(players[i-1]);
+    //         }
+    //         if(i+1 == players.length){
+    //             farr.push(players[i]);
+    //         }
+    //     }
+    //     players = farr;
+    // });
+    // return players;
+
+    ///다섯번째 시도.....
     let counters = {};
     callings.forEach(i => {
-        let thisi = counters[i];
-        if(thisi == undefined){
+        if(counters[i] == undefined){
             counters[i] = 1;
         }else{
             counters[i] += 1;
         }
     });
-
-    // Object.keys(counters).forEach((ite)=>{
-        
-    //     let front = players.slice(0, );
-    //     let back = [];
-    //     console.log(players.indexOf(ite));
-
-    // })
-    let newplayers = [];
-    Object.keys(counters).forEach((ckey)=>{
-        let ckeyIndex = players.indexOf(ckey);
-        for (let i = 0; i < players.length; i++){
-            const element = players[i];
-    
-            
+    Object.keys(counters).forEach((cai)=>{
+        console.log(cai, counters[cai])
+        let farr = [];
+        for (let i = counters[cai]; i < players.length; i++) {
+            if( players[i] == cai){
+                farr.push(cai, players[i-1]);
+                //여기서 찾으면 행동할걸 적어줘야해
+                i+= counters[cai];
+            }else{
+                farr.push(players[i-1]);
+            }
         }
-    })
-    // return newplay;
+        players = farr;
+    });
 }
 console.log(solution(["mumu", "soe", "poe", "kai", "mine"], ["kai", "kai", "mine", "mine"]));
